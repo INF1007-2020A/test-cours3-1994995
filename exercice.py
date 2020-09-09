11762089a72f0e70a9a7ffe39379165c79e8c1cd
1994995 <mat.fabre.teo@gmail.com>
2020-09-09 16:49:53 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -5,7 +5,7 @@ def capitaliser_pays(nom):
     nom_liste = str.split(nom)
     for lettre in range(len(nom_liste)):
         ascii_int = ord(lettre)
-        if ascii_int =< 
+        if 65ascii_int =< 
     return nom
 
 
